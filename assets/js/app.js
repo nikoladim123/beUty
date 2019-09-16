04e1c9aa382fa95d50c90bb8de6b490235c573d6
unknown <nikoladimitrijevic1991@gmail.com>
2019-09-16 20:23:40 +0200

    asdda

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+// arrow to top
+var arrowToTop = document.getElementsByClassName('arrowToTop');
+function arrowToTopFun() {
+  if(sectionTwo[0].getBoundingClientRect().top - window.innerHeight /1.2 <= 0 ){
+    arrowToTop[0].style.opacity = '1'
+  }else{
+    arrowToTop[0].style.opacity = '0'
+  }
+}
+
 // double hover
 var sectionTwo = document.getElementsByClassName('sectionTwo');
 var secTwoOverlay = document.getElementsByClassName('secTwoOverlay');
@@ -10,6 +20,21 @@ function sectionTwoDoubleHover() {
   }
 }
 
+// double hoveer sec three
+var sectionThree = document.getElementsByClassName('sectionThree');
+
+function sectionThreeDoubleHover() {
+  if(sectionThree[0].getBoundingClientRect().top - window.innerHeight /3 <= 0 ){
+    sectionThree[0].style.background = '#f5f6f7'
+  }else{
+    sectionThree[0].style.background = '#bfafa4'
+  }
+}
+
+
+// window listener
 window.addEventListener('scroll',()=>{
+  arrowToTopFun();
   sectionTwoDoubleHover();
+  sectionThreeDoubleHover();
 })
